32764ef4c17be04948c61174c1687054c357966d
hwanbit <hwanbit@gmail.com>
2024-09-26 16:04:08 +0900

    useCounter 훅

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './ch03/Book.css';
-import './ch04/Button.css';
-import './ch04/Clock.css';
-import './ch06/Notification.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Library from "./ch03/Library";
 import Clock from "./ch04/Clock";
 import CommentList from "./ch05/CommentList";
 import NotificationList from "./ch06/NotificationList";
+import Accommodate from "./ch07/Accommodate";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 setInterval(
     ()=>{
         root.render(
             <React.StrictMode>
-                <NotificationList />
+                <Accommodate />
             </React.StrictMode>
         );
     }
