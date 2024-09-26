101ff0da6bb76c279f634a78d25b58718c649f66
hwanbit <hwanbit@gmail.com>
2024-09-26 16:01:12 +0900

    useCounter 훅

--- src/ch06/NotificationList.jsx
+++ src/ch06/NotificationList.jsx
@@ -3,12 +3,15 @@ import Notification from "./Notification";
 
 const reservedNotifications = [
     {
+        id : 1,
         message : "안녕하세요~ 오늘 일정을 알려드립니다.",
     },
     {
+        id : 2,
         message : "현재 수업중인 웹프로그래밍활용 수업을 열심히 들으세요!!!",
     },
     {
+        id : 3,
         message : "열심히 듣고 Git에 Commit한 후에 하교하시면 됩니다.",
     },
 ];
@@ -34,6 +37,9 @@ class NotificationList extends React.Component {
                     notifications: notifications,
                 });
             } else {
+                this.setState({
+                    notifications: [],
+                });
                 clearInterval(timer); //interval 중지
             }
         }, 1000);
@@ -49,7 +55,10 @@ class NotificationList extends React.Component {
             <div>
                 {
                     this.state.notifications.map((notification) => { //첫번째 것을 덮어 두번째 것이 나타남
-                        return <Notification message={notification.message} />;
+                        return <Notification
+                            key = {notification.id}
+                            id = {notification.id}
+                            message={notification.message} />;
                     })
                 }
             </div>
